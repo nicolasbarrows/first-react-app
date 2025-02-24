fd430d75e49f8c07980bdefa57a5f3b3b2d739c8
Nicolas Barrows <contact@nicolasbarrows.com>
2025-02-24 15:55:50 -0600

    removed stock react demo text. moved new additions from 'body' to 'header'

--- src/App.js
+++ src/App.js
@@ -5,28 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. this is a test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <body>
         <h1>Nicolas Barrows</h1>
-        <p>\Personal top albums of 2025</p>
+        <p>
+          <i>Personal top albums of 2025</i>
+        </p>
         <ul>
           <li>WARNING HIGH CUBE by Moony</li>
           <li>CRACK A SMILE COME ON STAY AWHILE by Abby Holliday</li>
           <li>AMERICAN MOTOR SPORTS (420CC EDITION) by Bilmuri</li>
         </ul>
-      </body>
+        <img src={logo} alt="React logo" width="100" height="100" />
+      </header>
     </div>
   );
 }
