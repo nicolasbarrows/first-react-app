4ec960aa05655d7a0355220cf3eada5433a1ae90
Nicolas Barrows <contact@nicolasbarrows.com>
2025-02-24 15:32:21 -0600

    basic react app created and App.js edited.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. this is a test.
         </p>
         <a
           className="App-link"
@@ -18,6 +18,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <h1>Nicolas Barrows</h1>
+        <p>\Personal top albums of 2025</p>
+        <ul>
+          <li>WARNING HIGH CUBE by Moony</li>
+          <li>CRACK A SMILE COME ON STAY AWHILE by Abby Holliday</li>
+          <li>AMERICAN MOTOR SPORTS (420CC EDITION) by Bilmuri</li>
+        </ul>
+      </body>
     </div>
   );
 }
