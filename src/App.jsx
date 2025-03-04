3cc101a4d5eeaac49195bcc0b8b07ce25db9c759
Nicolas Barrows <contact@nicolasbarrows.com>
2025-03-04 08:58:45 -0600

    Completed React Hooks Exercise. Removed "album" redundant functions & language in favor of generic "lists"

--- src/App.jsx
+++ src/App.jsx
@@ -5,121 +5,69 @@ import ListGroup from "./shared/ListGroup";
 import ListItem from "./shared/ListItem";
 import Button from "./shared/Button";
 import JokesList from "./components/JokesList";
-import React, { Component } from "react";
+import { React, useState } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App(props) {
+  let [list, setList] = useState(["ready", "set", "GO"]);
+  let [text, setText] = useState("");
 
-    this.state = {
-      albums: [
-        "WARNING HIGH CUBE",
-        "CRACK A SMILE COME ON STAY AWHILE",
-        "AMERICAN MOTOR SPORTS (420CC EDITION)",
-      ],
-      album: "",
-      list: ["ready", "set", "GO"],
-      text: "",
-    };
-
-    this.onSubmit = this.onSubmit.bind(this);
+  function removeFromList(itemToRemove) {
+    let updatedList = list.filter((item) => item !== itemToRemove);
+    setList(updatedList);
   }
 
-  handleInput = (event) => {
-    this.setState({ album: event.target.value });
-  };
-
-  addAlbum = (event) => {
+  function onSubmit(event) {
     event.preventDefault();
-
-    this.setState({
-      albums: [...this.state.albums, this.state.album],
-      album: "",
-    });
-  };
-
-  removeAlbum = (selectedAlbum) => {
-    let updatedAlbums = this.state.albums.filter(
-      (album) => album !== selectedAlbum
-    );
-    this.setState({ albums: updatedAlbums });
-  };
-
-  onSubmit(event) {
-    event.preventDefault();
-    this.setState((prevState) => ({
-      list: [...prevState.list, prevState.text],
-      text: "",
-    }));
+    setList([...list, text]);
+    setText("");
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div>
-            <h1>Nicolas Barrows</h1>
-            <p>
-              <i>Personal top albums of 2025</i>
-            </p>
-            <ListGroup>
-              {this.state.albums.map((name, idx) => {
-                return (
-                  <ListItem key={name + idx}>
-                    <span>{name}</span>
-                    <Button
-                      className="square"
-                      onClick={(event) => this.removeAlbum(name)}
-                    >
-                      X
-                    </Button>
-                  </ListItem>
-                );
-              })}
-            </ListGroup>
-          </div>
-          <div>
-            <form onSubmit={this.addAlbum}>
-              <label htmlFor="task">Add New Album</label>
-              <input
-                type="text"
-                name="username"
-                id="task"
-                value={this.state.task}
-                onChange={this.handleInput}
-              />
-              <Button type="submit">Test</Button>
-            </form>
-          </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>Nicolas Barrows</h1>
+          <p>
+            <i>React List Project</i>
+          </p>
+        </div>
 
-          <div>
-            <h1>Hello World!</h1>
-            <form onSubmit={this.onSubmit}>
-              <input
-                type="text"
-                name="text"
-                value={this.state.text}
-                onChange={(event) => {
-                  this.setState({ text: event.target.value });
-                }}
-              ></input>
-              <Button type="submit">Add</Button>
-            </form>
-            <ul>
-              {this.state.list.map((item, idx) => {
-                return <li key={item + idx}>{item}</li>;
-              })}
-            </ul>
-          </div>
+        <div>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="text"
+              value={text}
+              onChange={(event) => {
+                setText(event.target.value);
+              }}
+            ></input>
+            <Button type="submit">Add</Button>
+          </form>
+          <ul>
+            {list.map((item, idx) => {
+              return (
+                <ListItem key={item + idx}>
+                  <span>{item}</span>
+                  <Button
+                    className="square"
+                    onClick={(event) => removeFromList(item)}
+                  >
+                    X
+                  </Button>
+                </ListItem>
+              );
+            })}
+          </ul>
+        </div>
 
-          <div>
-            <JokesList></JokesList>
-          </div>
-          <img src={logo} alt="React logo" width="100" height="100" />
-        </header>
-      </div>
-    );
-  }
+        <div>
+          <JokesList></JokesList>
+        </div>
+        <img src={logo} alt="React logo" width="100" height="100" />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/components/JokesList.jsx
+++ src/components/JokesList.jsx
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import ListGroup from "../shared/ListGroup";
 import ListItem from "../shared/ListItem";
 
-class JokesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { jokes: ["test"], joke: "" };
-  }
+function JokesList(props) {
+  let [list, setList] = useState([]);
 
-  componentDidMount() {
-    this.getJokes();
-  }
+  useEffect(() => {
+    getJokes(10);
+  }, []);
 
-  getJokes() {
-    const nJokes = "10";
-    const url = `https://official-joke-api.appspot.com/jokes/random/${nJokes}`;
+  function getJokes(numberOfJokes) {
+    const url = `https://official-joke-api.appspot.com/jokes/random/${numberOfJokes}`;
     fetch(url)
       .then((res) => {
         return res.json();
@@ -23,23 +19,20 @@ class JokesList extends Component {
         const jokesArray = json.map(
           (joke) => `${joke.setup} ${joke.punchline}`
         );
-        this.setState({ jokes: jokesArray });
+        setList(jokesArray);
       })
       .catch((err) => {
         console.error(err);
       });
   }
 
-  render() {
-    console.log(this.state);
-    return (
-      <ListGroup type="ul">
-        {this.state.jokes.map((name, idx) => {
-          return <ListItem key={name + idx}>{name}</ListItem>;
-        })}
-      </ListGroup>
-    );
-  }
+  return (
+    <ListGroup type="ul">
+      {list.map((name, idx) => {
+        return <ListItem key={name + idx}>{name}</ListItem>;
+      })}
+    </ListGroup>
+  );
 }
 
 export default JokesList;
